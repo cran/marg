dc8f1d868e294352b10c411cab4ad00b47bc3f8c
Alessandra R. Brazzale <alessandra.brazzale@unimore.it>
2009-09-14 00:00:00 +0000

    version 1.1-3

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.First.lib <- function(libname, pkgname) 
+{
+  version <- as.character("1.1-3 (2009/09/14)")
+  cat("\n   Package \"marg\"", version, "\n")
+  cat("   Copyright (C) 2000-2009 A. R. Brazzale\n\n")
+  cat("This is free software, and you are welcome to redistribute\n")
+  cat("it and/or modify it under the terms of the GNU General\n")
+  cat("Public License published by the Free Software Foundation.\n")
+  cat("Package \"marg\" comes with ABSOLUTELY NO WARRANTY.\n\n")
+  cat("type `help(package=\"marg\")' for summary information\n")
+  require(survival)
+  invisible()
+}
